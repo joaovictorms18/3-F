5caedfa33c9656e3f1a7c4f7644ed36825af24bf
jotinha <111078730+joaovictorms18@users.noreply.github.com>
2024-06-20 09:40:29 -0300

    20/06

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".abaconteudo");
-const contador =document.querySelectorAll(".contador")
+const contador =document.querySelectorAll(".contador");
+
+const tempoobjetivo1=new Date (2025,2,025)
+const tempoobjetivo2=new Date (2025,5,025)
+const tempoobjetivo3=new Date (2025,8,025)
+const tempoobjetivo4=new Date (2025,10,025)
+let agora=new Date();
+contador[0].textContent=tempoobjetivo1-agora
+contador[1].textContent=tempoobjetivo2-agora
+contador[2].textContent=tempoobjetivo3-agora
+contador[3].textContent=tempoobjetivo4-agora
 
 
 for (let i = 0; i < botoes.length; i++){
@@ -16,4 +26,5 @@ for (let i = 0; i < botoes.length; i++){
 
     }
 }
+
 
