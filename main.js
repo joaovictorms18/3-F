2e8eb95812a4bcf6c925b634a1f3acb938186d91
jotinha <111078730+joaovictorms18@users.noreply.github.com>
2024-09-06 08:51:03 -0300

    06/09

--- main.js
+++ main.js
@@ -35,16 +35,19 @@ function calculaTempo(tempoObjetivo) {
     minutos %= 60;
     horas %= 24;
     if (tempoFinal > 0){
-        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+        return [dias, horas, minutos, segundos]
     } else {
         return "Prazo Finalizado";
     }
 }
 
 function atualizaCronometro(){
-       
+       document.getElementById('dia0').textContent = calculaTempo(tempos[0])[0]
+       document.getElementById('hora0').textContent = calculaTempo(tempos[0])[1]
+       document.getElementById('minuto0').textContent = calculaTempo(tempos[0])[2]
+       document.getElementById('segundo0').textContent = calculaTempo(tempos[0])[3]
     for (let i=0; i<contadores.length;i++){
-        contadores[i].textContent = calculaTempo(tempos[i]);  
+        //contadores[i].textContent = calculaTempo(tempos[i]);  
     }
 }
 
@@ -53,4 +56,4 @@ function comecaCronometro(){
     setInterval(atualizaCronometro,1000);
 }
 
-//comecaCronometro();
+comecaCronometro();
